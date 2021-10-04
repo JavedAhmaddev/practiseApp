ae56d368482263bf153d605ab7c4b5b7c8252107
JavedAhmaddev <javaedjutt33@gmail.com>
2021-10-04 13:03:53 +0500

    Axios practise class with function based component

--- src/components/AxiosPractise.js
+++ src/components/AxiosPractise.js
@@ -1,64 +1,53 @@
-import React, { Component } from 'react'
-import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
 
+function AxiosPractise() {
+    const [posts, setPosts] = useState([]);
 
-
-export class AxiosPractise extends Component {
-
-    state={
-        posts: []
-    }
-
-
-    componentDidMount(){
+    useEffect(() => {
         axios.get('https://jsonplaceholder.typicode.com/posts')
         .then(res =>{
-            console.log("home:", res.data);
-            this.setState({
-                posts: res.data.slice(0,10)
-                
-            })
             
+                setPosts(res.data.slice(0,10));
+                
         })
-    }
-    
-    render() {
-        const {posts}= this.state;
-        console.log(posts);
-        const postlist = posts.length ? (
-            posts.map(post=>{
+    }, [])
 
-               return  <div className="center card" key={post.id} >
 
-                    <div className="card-content">
-                        <span className="card-title"> {post.title}</span>
-                        <p>{post.body} </p>
-                    </div>
-                </div>
-            })
-        ):(
-            <div className="center"> No Posts </div>
-        )
-        return (
-            <div>
-                <h1> Axios practise component </h1>
-                {postlist}
-            </div>
-        )
-    }
+    const postlist = posts.length ? (
+                    posts.map(post=>{
+        
+                       return  <div className="center card" key={post.id} >
+        
+                            <div className="card-content">
+                                <span className="card-title"> {post.title}</span>
+                                <p>{post.body} </p>
+                            </div>
+                        </div>
+                    })
+                ):(
+                    <div className="center"> No Posts </div>
+                )
+
+
+
+    return (
+        <div>
+             <h1> Axios practise component </h1>
+               {postlist}
+        </div>
+    )
 }
 
 export default AxiosPractise
 
 
-// using an arrow function inside componentdidmount and calling it as () 
-// componentDidMount() {
+// using an arrow function inside useEffect and calling it as () 
+// useEffect() {
 //     (async () => {
 //       let res = await axios.get("https://jsonplaceholder.typicode.com/posts");
 //       let posts = res.data;
 //         console.log(posts)
-//       this.setState({
-//         posts: posts,
-//       });
+//       setPosts(posts)
 //     })();
 //   }
